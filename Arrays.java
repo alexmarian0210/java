9ec38eeca6472a16cfa4f8a93943152a98ba7cdf
alexmarian0210 <alex_marian@estudante.sesisenai.org.br>
2026-02-11 20:19:22 -0300

    \zsf

---
+++ Arrays.java
@@ -0,0 +1,1 @@
+public class Arrays { public static void main(String[] args) { System.out.println("Funcionando"); } }
---
+++ app/src/Arrays.java
@@ -0,0 +1,20 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+
+//                  0  1  2  3  4  5  6  7  8  9
+ // int [] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+//for (int i = 0; i < numeros.length; i++) {
+  //  System.out.println(numeros[i]);
+
+  int [] [] matriz2 = new int [3] [3];
+
+    int[] [] matriz = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+    };
+    System.out.println(matriz2[1][2]);
+}
+    }
